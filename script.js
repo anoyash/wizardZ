22bf39ccf686e97868d1ecec7b0a16db920953d9
Ashutosh Kumar <ak2417942@gmail.com>
2024-12-16 23:19:52 +0530

    page1 customer logo content created with gsap animation

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+var customersLogo = document.querySelectorAll("#customers img");
 var page1Timeline = gsap.timeline();
+
+console.log(customersLogo);
+
+customersLogo.forEach((imgTag)=>{
+  console.log(imgTag);
+})
 
 page1Timeline.from("#logo", {
   y: -20,
@@ -12,17 +19,25 @@ page1Timeline.from("#nav-items", {
   opacity: 0,
 });
 
-gsap.from("#page2leftbrand", {
-  transform: "translateX(-50%)",
-  opacity: 0,
-  duration: 0.8,
-  stagger: 1,
-  delay: 1,
-});
+/* Writing this way will make sure my animation for page2leftbrand and page2right will run at the same time
+  instead of running one after another
+*/
+page1Timeline.add([
+  gsap.from("#page2leftbrand", {
+    transform: "translateX(-50%)",
+    opacity: 0,
+    duration: 0.8,
+    stagger: 1,
+  }),
+  gsap.from("#page2right img", {
+    transform: "translateX(50%)",
+    opacity: 0,
+    duration: 0.8,
+  }),
+]);
 
-gsap.from("#page2right img", {
-  transform: "translateX(50%)",
+page1Timeline.from("#customers img", {
+  y: 20,
   opacity: 0,
-  duration: 0.8,
-  delay: 1,
+  duration: 0.4,
 });
